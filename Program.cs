0461c94cf6bdaae03b739ac5c6ed6ba7d583c1a0
joshuaae12418-sys <joshuaae12418@gmail.com>
2026-03-13 16:34:07 +0800

    methods

--- Program.cs
+++ Program.cs
@@ -1,84 +1,116 @@
-﻿namespace airlineLoyaltycode
+﻿using System;
+namespace AirlineLoyaltyCode
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static string redeemCode = "";
+        static int Points = 0;
+        static bool exit = false;
+        static void Main()
         {
-
-            string redeemCode;
-            string points;
-            string editPoints;
-            string viewPoints;
-            string deletePoints;
-
 
             Console.Write("Write Your Name: ");
             string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name + " Welcome!!.");
-            Console.WriteLine("-----------------------------------");
-            Console.WriteLine("\nLOYALTY REDEEM POINTS\n");
-            Console.WriteLine("-----------------------------------");
-            Console.WriteLine("1. REDEEM POINTS");
-            Console.WriteLine("2. EDIT POINTS");
-            Console.WriteLine("3. VIEW POINTS");
-            Console.WriteLine("4. DELETE POINTS");
-            Console.WriteLine("5. WHERE TO FIND MY CODE");
-            Console.WriteLine("6. EXIT");
-            Console.Write("Enter Your Choice: ");
-            string choice = Console.ReadLine();
+            Console.WriteLine($"Hello {name}, Welcome!!");
 
-
-            switch (choice)
+            while (!exit)
             {
-                case "1":
-                    Console.WriteLine("Enter Redeem Code. Ex.45****7 ");
-                    redeemCode = Console.ReadLine();
 
-                    if (redeemCode != "")
-                    {
-                        Console.WriteLine("Redeem code is valid. Points redeemed successfully.");
-                        points = "50";
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid redeem code. Please try again.");
-                    }
-                    break;
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("\nLOYALTY REDEEM POINTS\n");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("1. REDEEM");
+                Console.WriteLine("2. EDIT");
+                Console.WriteLine("3. VIEW"); s
+                Console.WriteLine("4. DELETE");
+                Console.WriteLine("5. HELP");
+                Console.WriteLine("6. EXIT");
+                Console.Write("Enter Your Choice: ");
+                string choice = Console.ReadLine();
 
-                case "2":
-                    Console.Write("Enter No. of Points: ");
-                    points = Console.ReadLine();
-                    Console.WriteLine("Points have been edited successfully.");
-                    Console.WriteLine("Current Points: " + points);
-                    break;
-                
-                case "3":
-                    Console.WriteLine("Current Points: " + points);
-                    break;
-                
-                case "4":
-                    Console.WriteLine("Delete No. of Points: ");
-                    points = Console.ReadLine();   
-                    Console.WriteLine("Points Deleted Successfuly!");
-                    Console.WriteLine("Current Points: " + points);
-                    break;
-               
-                case "5":
-                    Console.WriteLine(@"________________________________________________
+
+                switch (choice)
+                {
+                    case "1":
+                        Redeem();
+                        break;
+
+                    case "2":
+                        Edit();
+                        break;
+
+                    case "3":
+                        ViewPoints();
+                        break;
+
+                    case "4":
+                        Delete();
+                        break;
+
+                    case "5":
+                        Help();
+                        break;
+
+                    case "6":
+                        Exit();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+
+                }
+            }
+
+            static void Redeem()
+            {
+                Console.WriteLine("Enter Redeem Code. Ex.45****7 ");
+                redeemCode = Console.ReadLine();
+
+                if (redeemCode == "1234567")
+                {
+                    Points += 50;
+                    Console.WriteLine("Points redeemed successfully!");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID CODE!!!");
+                }
+                Console.WriteLine("Current Points: " + Points);
+
+            }
+        }
+
+        static void Edit()
+        {
+            Console.Write("Enter No. of Points: ");
+            Points = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Points have been edited successfully.");
+        }
+        static void ViewPoints()
+        {
+            Console.WriteLine($"Current Points: {Points}");
+        }
+        static void Delete()
+        {
+            Console.WriteLine("Delete No. of Points: ");
+            Points = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Points Deleted Successfuly!");
+            Console.WriteLine("Current Points: " + Points);
+        }
+
+        static void Help()
+        {
+            Console.WriteLine(@"________________________________________________
 |  AIRLINE TICKET                                  |
 |  Name: JOSHUA                [ CODE: 45****7 ] <---|
 |__________________________________________________|");
-                    Console.WriteLine("");
-                    break;
-
-                case "6":
-                    Console.WriteLine("Exiting Program...");
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice. Please try again.");
-                    break;
-
-            }
+            Console.WriteLine("");
+        }
+        static void Exit()
+        {
+            Console.WriteLine("Exiting Program...");
+            exit = true;
         }
     }
 }
